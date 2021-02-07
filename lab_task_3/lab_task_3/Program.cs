f48f3fa4c31dbb82bf69b5b9df8722107130570b
Sabah Raad <raadsabah71@gmail.com>
2021-02-07 12:03:39 +0600

    lab_task_3.1

---
+++ lab_task_3/lab_task_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n=10;
+            
+            for (int i = 0; i <= n; i++)
+            {
+                if(i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+            }
+        }
+    }
