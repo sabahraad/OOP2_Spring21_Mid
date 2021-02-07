adb720569b665acd12e534f3b1dbe0ba66563423
Sabah Raad <raadsabah71@gmail.com>
2021-02-07 14:01:39 +0600

    not complete

---
+++ lab_task_3.4/lab_task_3.4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_task_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
