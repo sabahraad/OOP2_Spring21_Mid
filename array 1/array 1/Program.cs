888af0fba22a5aee4aed819fa9c8cf6c161af266
Sabah Raad <raadsabah71@gmail.com>
2021-02-14 14:03:43 +0600

    array1

---
+++ array 1/array 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[] { 1, 3, 6, 4, 2, 7, 6, 2, 3 };
+
+            Console.WriteLine("Duplicate elements: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        Console.WriteLine(arr[j]);
+                    }
+                }
+            }
+
+        }
+    }
+}
