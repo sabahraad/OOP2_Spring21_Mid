243b14727d3c4dd35cfb7e65fa1d84b4929f9365
Sabah Raad <raadsabah71@gmail.com>
2021-02-14 18:09:51 +0600

    array2

---
+++ array2/array2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 4,5,6,2,2,7,4};
+
+            Console.WriteLine("Unique elements: ");
+
+            for (int i = 0; i < arr.Length;i++)
+            {
+                int count = 0;
+                for (int  j=0; j < arr.Length; j++)
+                {
+                    
+                    if (arr[j] == arr[i])
+                    {
+                        count++;
+                    }
+                }
+                if (count == 1)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+
+
+        }
+    }
+}
